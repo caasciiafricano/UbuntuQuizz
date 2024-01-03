ce4739d5553acff9365b37805320c44206082db6
Carlos Teca Avelino <caasciiafricanopc.dev@gmail.com>
2024-01-03 15:25:58 +0100

    Deu ruim

---
+++ ubuntu-quizz/config/config.php
@@ -0,0 +1,7 @@
+<?php
+    define("MYSQL_HOST"     ,"localhost");
+    define("MYSQL_DBNAME"   ,"ubuntuquizz");
+    define("MYSQL_USER"     ,"root");
+    define("MYSQL_PASS"     ,"@CrackerCA@");
+    define("MYSQL_CHAR"     ,"utf8")
+?>
